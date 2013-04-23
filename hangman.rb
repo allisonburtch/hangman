5b7747e689e2ea90896f89561b6e3a126ac11255
allisonburtch <allisonburtch@gmail.com>
2013-04-23 18:38:52 -0400

    shit's working better yo

--- hangman.rb
+++ hangman.rb
@@ -1,36 +1,42 @@
 
+$wordList = ["abracadabra", "experimental", "lynx", "bicycle", "catamaran", "geography"]
+$n = rand($wordList.length)
+puts $wordList.length
+
+
 def prompt()
 	print "> "
 end
 
+	
 def guess_word()
+	puts $wordList[$n].to_s
 	puts "Do you want to guess the word?"
 	print prompt
 	guess_word = gets.chomp()
 	#this isn't working
-	if wordList[n].to_s == guess_word.to_s
+	if $wordList[$n].to_s == guess_word.to_s
 				puts "you win!"
 	else
-		puts man_drawing[i]
+		puts man_drawing[6]
 	end
 end
 
-
 def start()
-	wordList = ["abracadabra", "experimental", "lynx", "bicycle"]
-	n = rand(wordList.length)
+	# $wordList = ["abracadabra", "experimental", "lynx", "bicycle"]
+	# n = rand($wordList.length)
 	i = 0 # number of wrong guesses remaining
 	puts "we're going to play a game of hangman"
-	puts "The word has #{wordList[n].length} letters"
+	puts "The word has #{$wordList[$n].length} letters"
 
 	man_drawing = ["\n\t\t 0", "\n\t\t 0\n\t\t\\", "\n\t\t 0\n\t\t\\ /","\n\t\t 0\n\t\t\\ /\n\t\t |","\n\t\t 0\n\t\t\\ /\n\t\t |\n\t\t/","\n\t\t 0\n\t\t\\ /\n\t\t |\n\t\t/ \\"]	
 	next_move = []
 
 	while i < 6	
 		if next_move.length > 0
-			puts wordList[n].gsub(/[^#{next_move.join()}]/, " _")
+			puts $wordList[$n].gsub(/[^#{next_move.join()}]/, " _")
 		else
-			puts wordList[n].to_s.gsub(/[a-z]/, " _")
+			puts $wordList[$n].to_s.gsub(/[a-z]/, " _")
 		end
 
 		puts "Pick a letter"
@@ -39,9 +45,9 @@ def start()
 			#guesses.push.next_move
 		next_move << this_guess
 
-		if wordList[n].include? next_move.last
+		if $wordList[$n].include? next_move.last
 			# do nothing
-		# elsif wordList[n] == next_move[]
+		# elsif $wordList[n] == next_move[]
 		# 	puts "you win!"
 		else
 			i += 1
@@ -50,10 +56,10 @@ def start()
 		end
 	end
 
-	guess_word()
 end
 
 start()
+guess_word()
 
 
 
